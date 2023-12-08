80565da7e61c6d3626208fe6c1294af8099026c8
yangxue12 <2325449412@qq.com>
2023-12-08 23:05:33 +0800

    Update nft-minter.js

--- backend/nft-minter.js
+++ backend/nft-minter.js
@@ -5,8 +5,8 @@ export async function mint(to,uri){
     const provider = new JsonRpcProvider("http://localhost:8545");
     const signer = await provider.getSigner();
     const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-    const abi = JSON.parse(fs.readFileSync(",/abis/MyNFT.json"));
+    const abi = JSON.parse(fs.readFileSync("./abis/MyNFT.json"));
     const contract = new ethers.Contract(contractAddress,abi,signer);
-    const result = await contract.safeMint(to,url);
+    const result = await contract.safeMint(to,uri);
     console.log(result.hash);
 }
